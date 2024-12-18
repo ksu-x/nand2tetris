b430d4811589104b5c7847a96675116d5c0f35a4
xiaoqi <xiaoqi@eanstl.com>
2024-12-18 11:11:24 +0800

    Changes

--- nand2tetris-open-source-2.5.7/SimulatorsPackageSource/Hack/HardwareSimulator/PartPins.java
+++ nand2tetris-open-source-2.5.7/SimulatorsPackageSource/Hack/HardwareSimulator/PartPins.java
@@ -65,9 +65,9 @@ public class PartPins extends ValueComputerPart {
         partGateClass = null;
 
         // remove all node gui adapters
-        Enumeration enum = nodes.keys();
-        while (enum.hasMoreElements()) {
-            Node node = (Node)enum.nextElement();
+        Enumeration myEnum = nodes.keys();
+        while (myEnum.hasMoreElements()) {
+            Node node = (Node)myEnum.nextElement();
             Node nodeAdapter = (Node)nodes.get(node);
             node.removeListener(nodeAdapter);
         }
